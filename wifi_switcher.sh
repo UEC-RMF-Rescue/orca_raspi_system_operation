ccfe393d06a22b896cd6f38aaac2ba78a3f4b4a8
Hibiki Maruyama <144889491+MAMMARU-class@users.noreply.github.com>
2025-06-25 00:28:18 +0900

    successed to change ip addr

--- wifi_switcher.sh
+++ wifi_switcher.sh
@@ -6,13 +6,27 @@ ROUTER_PASS="Maruh1b1k1"
 WIFI_SSID="hibiki"
 WIFI_PASS="Maruh1b1k1"
 
+connect_wifi () {
+	wlx_iface=$(/usr/sbin/ip -o link show | grep "wlx" | awk -F': ' '{print $2}')
+        /usr/sbin/ip link set "$INTERNAL_IF" down
+	/usr/sbin/ip link set "$wlx_iface" up
+	/usr/bin/nmcli connection add type wifi con-name orca_wifi ifname $wlx_iface ssid $ROUTER_SSID
+	/usr/bin/nmcli connection modify orca_wifi wifi-sec.key-mgmt wpa-psk wifi-sec.psk $ROUTER_PASS
+	/usr/bin/nmcli connection modify orca_wifi ipv4.method manual\
+		ipv4.address 192.168.1.114/24\
+		ipv4.gateway 192.168.1.255\
+		ipv4.dns 8.8.8.8
+	/usr/bin/nmcli connection up orca_wifi
+}
+
+test_func () {
+    echo "hello world"
+}
+
 if [ "$1" = "add" ]; then
         logger "[udev] wifi dongle detected (0411:029b)"
-	wlx_iface=$(/usr/sbin/ip -o link show | grep "wlx" | awk -F': ' '{print $2}')
-	/usr/sbin/ip link set "$INTERNAL_IF" down
-	/usr/sbin/ip link set "$wlx_iface" up
-        /usr/bin/nmcli device wifi connect $ROUTER_SSID password $ROUTER_PASS
-	logger "[udev] wifi dongle enabled ($wlx_iface)"
+	connect_wifi
+	logger "[udev] wifi dongle enabled"
 	sudo systemctl start led_pink.service
 
 elif [ "$1" = "remove" ]; then
@@ -23,11 +37,8 @@ elif [ "$1" = "remove" ]; then
 
 elif [ "$1" = "external" ]; then
         logger "[udev] wifi dongle detected (0411:029b)"
-	wlx_iface=$(/usr/sbin/ip -o link show | grep "wlx" | awk -F': ' '{print $2}')
-	/usr/sbin/ip link set "$INTERNAL_IF" down
-	/usr/sbin/ip link set "$wlx_iface" up
-        /usr/bin/nmcli device wifi connect $ROUTER_SSID password $ROUTER_PASS
-	logger "[udev] wifi dongle enabled ($wlx_iface)"
+        connect_wifi
+	logger "[udev] wifi dongle enabled"
 	
 elif [ "$1" = "internal" ]; then
 	logger "[udev] wifi dongle not detected. enable internal Wifi ($INTERNAL_IF)"
